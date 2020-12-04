0b0b4c4e5f2affc94f636404ff664ac3fd279997
attila-h <hauth.attila@gmail.com>
2020-12-04 13:11:50 +0100

    add an axios fetch to App for testing purpose

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import Card from './components/card/Card'
+import { useState } from 'react';
+import Card from './components/card/Card';
+import axios from "axios";
 
-function App() {
-  return (
+const App = () => {
+  
+  const [card, setCard] = useState(null);
+
+  if (!card) {
+
+    axios.get(`http://localhost:8762/api/hero/get/69`)
+      .then((response) => {
+        setCard(response.data);
+      })
+      .catch((err) => {
+        console.log(err)
+      });
+    
+  }
+    
+  console.log(card)
+
+  return (    
     <div className="App">
       <div style={{margin: '100px'}}>
-        <Card isFlippable={true} isZoomable={true}/>
+        {card && <Card isFlippable={true} isZoomable={true} card={card}/>}
+        
       </div>
     </div>
   );
