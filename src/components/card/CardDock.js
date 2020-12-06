0aa9044d36780ddaeae061b84851ccda362df3f1
attila-h <hauth.attila@gmail.com>
2020-12-06 12:56:31 +0100

    add card dock rfc

--- src/components/card/CardDock.js
+++ src/components/card/CardDock.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import PropTypes from "prop-types";
 
-function CardDock() {
+function CardDock(props) {
+
+  const getStyle = () => {
+    return {
+      margin: props.margin
+    }
+  }
+
   return (
-    <div>
-      
+    <div className="card-dock" style={getStyle()}>
+      {props.children}
     </div>
   )
 }
 
 export default CardDock;
+
+CardDock.propTypes = {
+  margin: PropTypes.string,
+};
+
+CardDock.defaultProps = {
+  margin: "10px",
+};
