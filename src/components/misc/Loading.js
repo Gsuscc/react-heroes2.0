1c7dbacfb111922b52a9d22a10f9d8d66e02f3be
attila-h <hauth.attila@gmail.com>
2020-12-07 11:09:40 +0100

    fix loading timeout cleanup

--- src/components/misc/Loading.js
+++ src/components/misc/Loading.js
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Loading.css';
 
 const Loading = () => {
   const [dots, setDots] = useState('');
 
-  setTimeout(() => {
-    const newDots = dots === '...' ? '' : dots + '.';
-    setDots(newDots)
-  }, 500)
+  useEffect(() => {
+    const dotCycler = setTimeout(() => {
+      const newDots = dots === '...' ? '' : dots + '.';
+      setDots(newDots)
+    }, 500)
+    return () => {
+      clearTimeout(dotCycler)
+    }
+  }, [dots])
+
 
   return (
     <div className='loading'>
