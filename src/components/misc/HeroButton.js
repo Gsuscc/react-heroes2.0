f41e770678da3c20a37327d61e5487a8ce857abf
attila-h <hauth.attila@gmail.com>
2020-12-08 09:01:32 +0100

    add margin to form container

--- src/components/misc/HeroButton.js
+++ src/components/misc/HeroButton.js
@@ -3,25 +3,26 @@ import PropTypes from "prop-types";
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles({
-  root: {
-    fontFamily: 'inherit',
-    fontSize: '2em',
-    padding: '0px 20px',
-    borderRadius: '5px',
-    letterSpacing: '3px',
-    color: 'orange',
-    WebkitTextStrokeWidth: '2px',
-    WebkitTextStrokeColor: 'black',
-    transition: 'color 0.5s',
-    '&:hover': {
-      color: 'white',
-      background: 'initial'
-    } 
-  },
-});
-
 function HeroButton(props) {
+
+  const useStyles = makeStyles({
+    root: {
+      fontFamily: 'inherit',
+      fontSize: `2em`,
+      padding: '0px 20px',
+      borderRadius: '5px',
+      letterSpacing: '3px',
+      color: 'orange',
+      WebkitTextStrokeWidth: '2px',
+      WebkitTextStrokeColor: 'black',
+      transition: 'color 0.5s',
+      '&:hover': {
+        color: 'white',
+        background: 'initial'
+      },
+    },
+  });
+
   const classes = useStyles();
 
   return (
