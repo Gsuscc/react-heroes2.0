c6cdd8f1cedd138d16ffce942b911fc1aea772b3
Gergely Molnar <molnar.gergo18@gmail.com>
2020-12-12 13:09:40 +0100

    Enjoy the winter with fancy snowfall

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Snowfall from 'react-snowfall'
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+      <Snowfall style={{ zIndex: '1000' }}/>
       <App />
   </React.StrictMode>,
   document.getElementById('root')
