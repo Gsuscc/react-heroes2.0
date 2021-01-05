baee443fa3f484ac36f7613e830382f102bbb6b0
Gergely Molnar <molnar.gergo18@gmail.com>
2021-01-05 13:16:06 +0100

    add army hideable army slot

---
+++ src/components/cardpage/ArmySlot.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './ArmySlot.css'
+
+const ArmySlot = () => {
+    return (
+        <div className="slots">
+            <div className="slot"></div>
+            <div className="slot"></div>
+            <div className="slot"></div>
+            <div className="slot"></div>
+            <div className="slot"></div>
+        </div>
+    )
+}
+export default ArmySlot
--- src/components/cardpage/MyCards.js
+++ src/components/cardpage/MyCards.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef, useContext } from "react";
+import React, { useEffect, useState, useRef, useContext, useCallback } from "react";
 import Card from "../card/Card";
 import axios from "axios";
 import CardDock from "../card/CardDock";
@@ -9,6 +9,9 @@ import { GlobalContext } from "../../state/GlobalState";
 import InfoText from "../misc/InfoText";
 import CardContainer from "../card/CardContainer";
 import LoginCheck from "../misc/LoginCheck";
+import ArmySlot from "./ArmySlot";
+import Button from '@material-ui/core/Button';
+
 
 const MyCards = (props) => {
   return (
@@ -25,6 +28,14 @@ const MyCardsComponent = () => {
   const [heroesList, setHeroesList] = useState([]);
   const pageBottom = useRef();
   const { addNewAlert } = useContext(GlobalContext);
+  const [isArmySlotVisible, setIsArmySlotVisible] = useState(false)
+
+  const toggleSlots = useCallback(
+    () => {
+      setIsArmySlotVisible(!isArmySlotVisible)
+    },
+    [isArmySlotVisible],
+  )
 
   useEffect(() => {
     const toggleDiv = pageBottom.current;
@@ -92,7 +103,16 @@ const MyCardsComponent = () => {
           : !isLoading && (
               <InfoText>No cards, go to Shop to collect'em</InfoText>
             )}
+      {isArmySlotVisible && <ArmySlot/>}
       </CardContainer>
+      <React.Fragment>
+            <div class="bounce">
+            <Button className="downArrow" variant="contained" color="primary" onClick={toggleSlots}>
+                Slots
+            </Button>
+              {/* <img className="downArrow" width="80" height="80" alt="" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/PjwhRE9DVFlQRSBzdmcgIFBVQkxJQyAnLS8vVzNDLy9EVEQgU1ZHIDEuMS8vRU4nICAnaHR0cDovL3d3dy53My5vcmcvR3JhcGhpY3MvU1ZHLzEuMS9EVEQvc3ZnMTEuZHRkJz48c3ZnIGVuYWJsZS1iYWNrZ3JvdW5kPSJuZXcgMCAwIDMyIDMyIiBoZWlnaHQ9IjMycHgiIGlkPSLQodC70L7QuV8xIiB2ZXJzaW9uPSIxLjEiIHZpZXdCb3g9IjAgMCAzMiAzMiIgd2lkdGg9IjMycHgiIHhtbDpzcGFjZT0icHJlc2VydmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiPjxwYXRoIGQ9Ik0yNC4yODUsMTEuMjg0TDE2LDE5LjU3MWwtOC4yODUtOC4yODhjLTAuMzk1LTAuMzk1LTEuMDM0LTAuMzk1LTEuNDI5LDAgIGMtMC4zOTQsMC4zOTUtMC4zOTQsMS4wMzUsMCwxLjQzbDguOTk5LDkuMDAybDAsMGwwLDBjMC4zOTQsMC4zOTUsMS4wMzQsMC4zOTUsMS40MjgsMGw4Ljk5OS05LjAwMiAgYzAuMzk0LTAuMzk1LDAuMzk0LTEuMDM2LDAtMS40MzFDMjUuMzE5LDEwLjg4OSwyNC42NzksMTAuODg5LDI0LjI4NSwxMS4yODR6IiBmaWxsPSIjMTIxMzEzIiBpZD0iRXhwYW5kX01vcmUiLz48Zy8+PGcvPjxnLz48Zy8+PGcvPjxnLz48L3N2Zz4=" /> */}
+            </div>
+      </React.Fragment>
       <div
         className="scrollTrigger"
         ref={pageBottom}
