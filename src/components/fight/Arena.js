dd2d4c7adfa88c08d3156dc2d76234885c0891d7
Gergely Molnar <molnar.gergo18@gmail.com>
2021-01-25 17:30:24 +0100

    add flip all cards option for the attacker

--- src/components/fight/Arena.js
+++ src/components/fight/Arena.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useContext, useEffect, useState } from "react";
+import React, { createRef, useContext, useEffect, useRef, useState } from "react";
 import { GlobalContext } from "../../state/GlobalState";
 import Card from "../card/Card";
 import CardContainer from "../card/CardContainer";
@@ -27,6 +27,12 @@ const ArenaComponent = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [fightLog, setFightLog] = useState();
   const [isBattle, setIsBattle] = useState(false);
+  const defenderCards = useRef([])
+  const attackerCards = useRef([])
+
+  attackerCards.current = Array(5).fill().map(
+    (ref, index) =>   attackerCards.current[index] = createRef()
+  )
 
   useEffect(() => {
     axios
@@ -81,24 +87,34 @@ const ArenaComponent = () => {
       });
   };
 
+  const flipAll = (e) => {
+    attackerCards.current.forEach((el, i) => {
+      setTimeout(() => el.current.firstChild.click(), i*150)
+  }
+    )
+  }
+
   return (
     <React.Fragment>
       {isLoading && <Loading />}
       {!isLoading && isBattle && <Battle fightLog={fightLog} />}
       {!isLoading && !isBattle && (
         <React.Fragment>
+          <HeroButton onClick={flipAll}>Flip ally</HeroButton>
           <InfoText>{myArmy.nick}'s Army</InfoText>
           <div className="army-container">
-            {myArmy.cards.map((hero) => {
+            {myArmy.cards.map((hero, i) => {
               return (
                 <CardDock key={hero.uniqueId}>
-                  <Card
+                  <div ref={attackerCards.current[i]}>
+                   <Card
                     hero={hero}
                     isFlippable={true}
                     isZoomable={true}
                     isUserCard={true}
                     isRightClickabale={false}
-                  />
+                  /> 
+                  </div>
                 </CardDock>
               );
             })}
@@ -111,7 +127,7 @@ const ArenaComponent = () => {
               <div className="army-container">
                 {enemyArmy.cards.map((hero) => {
                   return (
-                    <CardDock key={hero.uniqueId}>
+                    <CardDock ref={defenderCards} key={hero.uniqueId}>
                       <Card
                         hero={hero}
                         isFlippable={true}
